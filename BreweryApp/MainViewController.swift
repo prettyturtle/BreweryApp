dcf1bc4e9794155fdfaafd9a44bf75cdbcbbd129
prettyturtle <unifconti@gmail.com>
2022-02-28 08:55:46 +0900

    맥주 리스트를 테이블 뷰로 나타내기(TODO: 커스텀 셀 만들기)

--- BreweryApp/MainViewController.swift
+++ BreweryApp/MainViewController.swift
@@ -11,9 +11,13 @@ import SnapKit
 class MainViewController: UIViewController {
 
     private let fetchData = FetchData()
+    private var breweryList: [Brewery] = []
     
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
+        
+        tableView.dataSource = self
+        tableView.register(MainTableViewCell.self, forCellReuseIdentifier: MainTableViewCell.identifier)
         
         return tableView
     }()
@@ -22,10 +26,32 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         setupNavigationItem()
         setupLayout()
+        fetch()
     }
     
 }
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return breweryList.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MainTableViewCell.identifier, for: indexPath) as? MainTableViewCell else { return UITableViewCell() }
+        cell.setupView(brewery: breweryList[indexPath.row])
+        return cell
+    }
+}
+
 private extension MainViewController {
+    func fetch() {
+        fetchData.fetch { [weak self] breweryList in
+            guard let self = self else { return }
+            self.breweryList = breweryList
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
     func setupLayout() {
         [
             tableView
--- README.md
+++ README.md
@@ -8,6 +8,8 @@
 ### 기능🍗
 
 - PunkAPI를 사용하여 맥주리스트를 만든다
+    - `UITableView`에서 커스텀 셀을 만들어 나타낸다
+    - TODO: 커스텀 셀 만들기
 
 ### FetchData🧀
 
